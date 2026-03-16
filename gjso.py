28de4909e3c4ebd973e8a2f8d6cf7ba2c2658106
MochiKoffe <sophiem47@nycstudents.net>
2026-03-16 10:16:49 -0400

    day 19

--- gjso.py
+++ gjso.py
@@ -1,4 +1,4 @@
-class Calculator():
+""" class Calculator():
     def add(x,y): 
         print(x+y)
         return x+y
@@ -7,12 +7,56 @@ class Calculator():
         return sum(numbers)
     def subtract (numbers):
         return numbers
-Calculator.add(5,6)
+Calculator.add(5,6) """
 
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory): 
         self.name = name
         self.money = money
         self.inventory = inventory
     def buy(self, item): 
-        
+        self.inventory.append(item)
+        print(self.inventory)
+
+Jillian = Hero("Jillian", 150, ["potion"])
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__ ) """
+
+""" class BankAccount: 
+    def __init__(self, owner, balance): 
+        self.owner = owner
+        self.__balance = balance 
+    def deposit(self, amount): 
+        self.__balance += amount
+    def show_balance(self): 
+        print(f"{self.owner}) has ${self.__balance}")
+
+sabrina = BankAccount("Sabrina", 2)
+sabrina.deposit({"amount": 67})
+print(sabrina.__dict__ ) """
+
+class Hero: 
+    def __init__(self, name, money, inventory): 
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+    def buy(self, item): 
+        self.inventory.append(item)
+        print(self.inventory)
+
+Isaac = Hero("Isaac", 50000, ["quill"])
+Isaac.buy({"title": "book", "pages": 90})
+print(Isaac.__dict__ )
+
+class Pet: 
+    def __init__(self, name, happiness): 
+        self.name = name
+        self.__happiness = happiness
+    def play(self, time): 
+        self.__happiness += int(time)
+    def show_status(self): 
+        print(f"{self.name}) is {self.__happiness}")
+
+Meow = Pet("Meow", 1)
+Meow.play(time = 9)
+print(Meow.__dict__ )
