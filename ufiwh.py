0287fa5b432704bedbc01eb8ce1e89427454cede
MochiKoffe <sophiem47@nycstudents.net>
2026-03-30 10:17:22 -0400

    day 23?

---
+++ ufiwh.py
@@ -0,0 +1,27 @@
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+
+def receipt(orders): 
+    the_receipt = {}
+    for sushi in orders: 
+        if sushi["name"] in the_receipt: 
+            continue
+        else: 
+            the_receipt[sushi["name"]] = {
+                "price": sushi["price"], 
+                "qty": 1
+            }
+    print(the_receipt)
+            
+
+receipt(sushi_orders)
