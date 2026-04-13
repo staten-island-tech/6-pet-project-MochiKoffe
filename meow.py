f26e1574df43a84cb4bc00cf0cd228d7cb9390ab
MochiKoffe <sophiem47@nycstudents.net>
2026-04-13 10:17:37 -0400

    h

--- meow.py
+++ meow.py
@@ -7,12 +7,12 @@ wards = {
 
 def departments(wards): 
     staff = {}
-    for dept, docs in wards.items(): 
-        for doc in docs: 
-            if doc not in staff: 
-                staff[doc] = [dept]
-            else: 
-                staff[doc].append(dept)
-    print(staff)
+    for dept, docs in wards.items(): #for the items in the wards
+        for doc in docs: #in all the doctors
+            if doc not in staff: #if the doctors arent in the dictionary, add them
+                staff[doc] = [dept] #format of dictionary, the doctor = [list of occupations]
+            else: #when it is in the dictionary
+                staff[doc].append(dept) #add all their occupations into the list by adding the depts
+    print(staff) #print the new dictionary
 
 departments(wards)
